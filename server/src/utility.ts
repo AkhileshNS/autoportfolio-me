c64171da96c5fb13f25e44a08f83e4ec272d1805
AkhileshNS <nsakhilesh02@gmail.com>
2020-10-02 13:59:00 +0530

    Updated prefiltering

--- server/src/utility.ts
+++ server/src/utility.ts
@@ -3,9 +3,11 @@ export const convertToStatements = (str: string | null) =>
     ? str
         .split('\n')
         .map((item: string) => item.trim())
-        .filter((item: string) => Boolean(item))
+        .filter(
+          (item: string) => Boolean(item) && item.toLowerCase() !== 'see less'
+        )
         .map((item) => {
-          if (item[0] === '-') {
+          if (!/[a-z]/i.test(item[0])) {
             return item.slice(2);
           }
           return item;
